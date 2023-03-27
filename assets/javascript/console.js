b639946ba4dc59ff2d3e73400edb14dfc3883702
Nidushan <apemanxd1@gmail.com>
2023-03-27 17:58:08 -0400

    console script

---
+++ assets/javascript/console.js
@@ -0,0 +1,9 @@
+const style = "font-weight: bold; font-size:20px;"
+var emoji = ["🐵", "🦍", "🍌"];
+var i = Math.floor(Math.random() * emoji.length);
+for (var x = 0; x < 1; x++) {
+  console.log("%c" + emoji[i] + " Developed by apeman", style);
+  console.log("%c" + emoji[i] + " apeman#0001 / https://discord.gg/apeman", style);
+  console.log("%c" + emoji[i] + " @TGapeman / https://t.me/TGapeman", style);
+
+}
