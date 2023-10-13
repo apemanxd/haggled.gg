9def431b751eda72069a1c2356e567e38b343a2e
Nidushan <apemanxd1@gmail.com>
2023-10-13 12:43:41 -0400

    update

--- calculator/fees/cdn/main.js
+++ calculator/fees/cdn/main.js
@@ -15,42 +15,38 @@ function functionOnce() {
 	if (!something) removeHRClass();
 }
 
-function calculate(input) {
-	functionOnce();
-	Price = Math.ceil(input);
-	percentage = (document.getElementById("percentageInput").value) / 100;
-	constFee = parseFloat(document.getElementById("constInput").value);
+function calculate() {
 
-	finalFee = ((percentage) * (Price + constFee)) + constFee;
-	finalFee = finalFee.toFixed(2);
-
-	finalPrice = Price - finalFee;
-	finalPrice = finalPrice.toFixed(2);
-
-	TotalCrypto = finalPrice;
-
-	finalWithFees = Price + (parseFloat(finalFee));
-	finalWithFees = Math.ceil(finalWithFees);
-
-	var FinalLetter = input;
-	FinalLetter = Number(FinalLetter).toLocaleString("en-EN").split(/\s/).join(',');
-
-
-	if (input >= 10000) {
-		var TotalPriceString = "YOU CRAZYYY";
-		var cryptoString = "PM me on https://t.me/TGapeman for special pricing and instructions 💛";
+		// Function logic
+		var inputAmount = parseFloat(document.getElementById("input").value);
+		var roundedPrice = Math.ceil(inputAmount);
+		
+		var percentage = parseFloat(document.getElementById("percentageInput").value) / 100;
+		var constFee = parseFloat(document.getElementById("constInput").value);
+		
+		var finalFee = ((percentage) * (roundedPrice + constFee)) + constFee;
+		var finalPrice = roundedPrice - finalFee;
+		var totalCrypto = finalPrice.toFixed(2);
+		
+		var finalWithFees = (roundedPrice + parseFloat(finalFee)).toFixed(2);
+		
+		// Format input amount with commas
+		var formattedInput = Number(inputAmount).toLocaleString("en-EN").split(/\s/).join(',');
+	
+		var TotalPriceString, cryptoString;
+	
+		if (inputAmount >= 10000) {
+			TotalPriceString = "YOU CRAZYYY";
+			cryptoString = "PM me on https://t.me/TGapeman for special pricing and instructions 💛";
+		} else {
+			TotalPriceString = "Exchange amount: <strong>$" + formattedInput + "</strong><br><span id='small'>The Fee: <strong>$" + finalFee.toFixed(2) + "</strong></span>";
+			cryptoString = "<strong>Receive:</strong> Amount you will receive ~ $" + totalCrypto + "<br><strong>Pay:</strong> If you want $" + formattedInput + ", then you have to send ~ $" + finalWithFees;
+		}
+	
+		// Update HTML elements
+		document.getElementById("price").innerHTML = TotalPriceString;
+		document.getElementById("crypto").innerHTML = cryptoString;
 	}
-
-
-	else {
-		var TotalPriceString = "Exchange amount: " + "<strong>$" + FinalLetter + "</strong>" + "<br><span id='small'>The Fee: <strong> $" + finalFee + "</strong></span>";
-		var cryptoString = "<strong>Receive:</strong> Amount you will receive ~ $" + TotalCrypto + "<br><strong>Pay:</strong> If you want $" + FinalLetter + ", then you have to send ~ $" + (finalWithFees);
-	}
-
-	document.getElementById("price").innerHTML = TotalPriceString;
-	document.getElementById("crypto").innerHTML = cryptoString;
-
-}
 
 function alertFees() {
 	alert("The fees for each payment method\nCrypto = $" + CryptoConstFees + "\nNB! All prices listed includes the fees.");
