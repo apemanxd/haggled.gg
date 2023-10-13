d48f6969a5c3b91fb30549a7ca622d4dfcbf8ec9
Nidushan <apemanxd1@gmail.com>
2023-10-13 12:15:55 -0400

    updated fees

--- calculator/credits/cdn/main.js
+++ calculator/credits/cdn/main.js
@@ -15,48 +15,41 @@ function functionOnce() {
 	if (!something) removeHRClass();
 }
 
-function calculate(input) {
-	functionOnce();
-	Price = (input);
-	percentage = (document.getElementById("percentageInput").value);
-	constFee = parseFloat(document.getElementById("constInput").value);
-
-	//Credit with fees
-	finalFee = (Price*100)/(100-percentage);
-	finalFee = finalFee.toFixed(2);
-	finalFee = Math.ceil(finalFee);
-
-	//Price with credit fees covered
-	finalPrice = ((constFee*finalFee)/1000).toFixed(2);
-	totalPrice = finalPrice;
-
-	//Price without credit fees covered
-	withoutFees = ((input*constFee)/1000).toFixed(2);
-	withoutFeesCredit = (input*((100-percentage)/100));
-
-	//Price for withoutcredit fees
-	withcreditfeeMoney= (withoutFeesCredit*constFee)/1000;
-	withcreditfeeCredit= (withoutFeesCredit*((100-percentage)/100)).toFixed(2);
-
-
-
-	if (input >= 2000000) {
-		var TotalPriceString = "YOU CRAZY";
-		var cryptoString = "PM me on <a href='https://t.me/TGapeman/'>t.me/TGapeman</a> for special pricing and instructions 💛";
+function calculate() {
+		// Get input values
+		var creditInput = parseFloat(document.getElementById("input").value);
+		var percentage = parseFloat(document.getElementById("percentageInput").value);
+		var constFee = parseFloat(document.getElementById("constInput").value);
+	
+		// Calculate final fee with credit fees
+		var finalFee = (creditInput * 100) / (100 - percentage);
+		finalFee = Math.ceil(finalFee);
+	
+		// Calculate prices
+		var finalPrice = ((constFee * finalFee) / 1000).toFixed(2);
+		var withoutFees = ((creditInput * constFee) / 1000).toFixed(2);
+		var withoutFeesCredit = (creditInput * ((100 - percentage) / 100));
+		var withCreditFeeMoney = ((withoutFeesCredit * constFee) / 1000).toFixed(2);
+		var withCreditFeeCredit = Math.ceil((withoutFeesCredit * ((100 - percentage) / 100)));
+	
+		// Create output strings
+		var totalPriceString, cryptoString;
+	
+		if (creditInput >= 2000000) {
+			totalPriceString = "YOU CRAZY";
+			cryptoString = "PM me on <a href='https://t.me/TGapeman/'>t.me/TGapeman</a> for special pricing and instructions 💛";
+		} else {
+			totalPriceString = "Credit amount: <strong>©" + creditInput + "</strong> | <span id='small'>With sending fees: <strong>©" + finalFee + "</strong></span>";
+			cryptoString = "<strong>Option 1:</strong> Pay $" + finalPrice + " to send ©" + finalFee + " and receive ©" + creditInput +
+				"<br><strong>Option 2:</strong> Pay $" + withoutFees + " to send ©" + creditInput + " and receive ©" + withoutFeesCredit +
+				"<br><strong>Option 3:</strong> Pay $ " + withCreditFeeMoney + " to send ©" + withoutFeesCredit + " and receive ©" + withCreditFeeCredit;
+		}
+	
+		// Update HTML
+		document.getElementById("price").innerHTML = totalPriceString;
+		document.getElementById("crypto").innerHTML = cryptoString;
 	}
-
-
-	else {
-		var TotalPriceString = "Credit amount: " + "<strong>©" + input + "</strong>" + " | <span id='small'>With sending fees: <strong> ©" + finalFee + "</strong></span>";
-		var cryptoString = "<strong>Option 1:</strong> Pay $" + totalPrice + " to send ©" + finalFee + " and receive ©"+ input + 
-		"<br><strong>Option 2:</strong> Pay $" + withoutFees + " to send ©" + input +" and receive ©" + withoutFeesCredit +
-		"<br><strong>Option 3:</strong> Pay $ " + withcreditfeeMoney + " to send ©" + withoutFeesCredit +" and receive ©" + withcreditfeeCredit;
-	}
-
-	document.getElementById("price").innerHTML = TotalPriceString;
-	document.getElementById("crypto").innerHTML = cryptoString;
-
-}
+	
 
 function alertFees() {
 	alert("The fees for each payment method\nCrypto = $" + CryptoConstFees + "\nNB! All prices listed includes the fees.");
